807c76b11e2a97210c7bf731b4d051c567fa7c01
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-04-30 19:12:43 +0200

    Added linting

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Map from '../Map/Map';
-import styles from './App.module.scss';
+import Map from "../Map/Map";
+import styles from "./App.module.scss";
 
 class App extends Component {
   render() {
     return (
       <div>
-       <Map />
+        <Map />
       </div>
     );
   }
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
-import styles from './Map.module.scss';
+import React, { Component } from "react";
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+
+import styles from "./Map.module.scss";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 class Map extends Component {
+  state = {};
 
-    componentDidMount() {
-     this.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10',
-        zoom: 11,
-        attributionControl: false,
-        center: [18.063240, 59.334591],
-      });
-    }
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v10",
+      zoom: 11,
+      attributionControl: false,
+      center: [18.06324, 59.334591]
+    });
+  }
+
+  onMapClick = () => {
+    this.map.on("click", e => {
+      console.log(e.lngLat);
+    });
+  };
 
   render() {
     return (
-      <div className={styles.map} id="map">
-       
+      <div className={styles.map} id="map" onClick={this.onMapClick}>
+        <p>{this.state.loading}</p>
       </div>
     );
   }
